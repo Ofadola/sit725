01a252b6f018b8334921e5101a11d2466ff66d3e
ofadola <busayofadola@yahoo.com>
2020-10-18 21:45:49 +1100

    i created a web service to add two numbers together

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+
+var adder=function(num1,num2){
+    var result = num1+num2;
+    return result
+}
+
+
+app.get('/test' ,function(req,res){
+    var username=req.query.username;
+    console.log('ye i ve been hit'+username);
+    res.send('hello you hit me! '+username);
+})
+
+app.get('/adder',function(req,res){
+    var num1=parseInt(req.query.num1);
+    var num2=parseInt(req.query.num2);
+    var result=adder(num1,num2)
+    res.send('The result is: '+result)
+})
+
+
+var port=3000;
+app.listen(port)
+console.log('server listening on: '+port)
